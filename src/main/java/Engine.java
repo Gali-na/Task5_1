0ff6a16eef68f40433e945f36243a8b044aabc0e
Tsivka Galina <galinatsivka@gmail.come>
2020-07-18 14:41:40 +0300

    first commit

---
+++ src/main/java/Engine.java
@@ -0,0 +1,9 @@
+public class Engine implements Cloneable {
+    private int engineCapacity;
+    private int power;
+    private int topSpeed;
+    private int accelerationTime;
+    private int cylinderDiameter;
+
+    
+}
